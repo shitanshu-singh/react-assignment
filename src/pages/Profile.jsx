8ee065aa999918f867a3b1500c82737630decb0f
shitanshu-singh <150735585+shitanshu-singh@users.noreply.github.com>
2023-11-24 00:47:28 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -196,23 +196,16 @@ const Profile = ({ userDetails, posts }) => {
             ))}
           </div>
           {modal && (
-            <>
-              <button onClick={toggleModal} className="btn-modal">
-                Open
-              </button>
-              {modal && (
-                <div className="modal">
-                  <div onClick={toggleModal} className="overlay"></div>
-                  <div className="modal-content">
-                    <h2>{postTitle}</h2>
-                    <p>{postBody}</p>
-                    <button className="close-modal" onClick={toggleModal}>
-                      CLOSE
-                    </button>
-                  </div>
-                </div>
-              )}
-            </>
+            <div className="modal">
+              <div onClick={toggleModal} className="overlay"></div>
+              <div className="modal-content">
+                <h2>{postTitle}</h2>
+                <p>{postBody}</p>
+                <button className="close-modal" onClick={toggleModal}>
+                  CLOSE
+                </button>
+              </div>
+            </div>
           )}
         </div>
       </div>
